102beaf0e37bf08fd4f67e0da066431af0c251fe
Chris <ChrisDev427@gmail.com>
2024-01-22 09:25:22 +0100

    update

--- website/js/websiteConfig.js
+++ website/js/websiteConfig.js
@@ -12,3 +12,39 @@ if ('ontouchstart' in window || navigator.maxTouchPoints) {
     console.log("The user is connecting from a desktop.");
     connectedFrom_desktop = true;
 }
+
+// function isLandscapeMode() {
+//     return window.innerWidth < window.innerHeight;
+// }
+  
+// function handleOrientationChange() {
+      
+//     const gameDiv = document.getElementById('gameDiv');
+//     const navbar = document.getElementById('navbar');
+//     const footer = document.getElementById('footer');
+      
+//     if (isLandscapeMode() && connectedFrom_mobile) {
+//         // Code to execute when the device is in landscape mode
+//         console.log('In landscape mode');
+//         gameDiv.style.maxWidth = '300px';
+//         navbar.classList.add('hidden-element');
+//         footer.classList.add('hidden-element');
+          
+          
+//     } else {
+//         // Code to execute when the device is not in landscape mode
+//         console.log('Not in landscape mode');
+//         gameDiv.style.maxWidth = '900px';
+//         navbar.classList.remove('hidden-element');
+//         footer.classList.remove('hidden-element');
+            
+            
+            
+//     }
+        
+// }
+//   // Add an event listener for the orientationchange event
+//   window.addEventListener('orientationchange', handleOrientationChange);
+  
+//   // Execute the handler on the initial page load
+//   handleOrientationChange();
