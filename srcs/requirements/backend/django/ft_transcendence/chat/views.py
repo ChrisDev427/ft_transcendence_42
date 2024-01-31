abde9c6ea35599ca9e82275d2ee53b901b44b4cb
AxelFdez <axe06@hotmail.fr>
2024-01-31 15:18:03 +0100

    add friends and chat backend

--- srcs/requirements/backend/django/ft_transcendence/chat/views.py
+++ srcs/requirements/backend/django/ft_transcendence/chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Chat
+from .models import Chat , Message
 from .serializers import ChatSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
 from account.models import UserProfile
@@ -27,30 +27,36 @@ class ChatView(APIView):
 			chat = Chat.objects.get(user1=user1, user2=user2)
 		except Chat.DoesNotExist:
 			return Response({"error", "chat not exist"}, status=status.HTTP_404_NOT_FOUND)
-		serializer = ChatSerializer(chat, many=True)
+		serializer = ChatSerializer(chat)
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MessageView(APIView):
 	permission_classes = [IsAuthenticated]
 
-	def put(self, request, format=None):
-		user = request.user
+	def put(self, request):
+		user = UserProfile.objects.get(user=request.user)
 		chat_id = request.data.get('chat_id')
 		message = request.data.get('message')
-		serializer = MessageSerializer(user, chat_id, message)
+		data =	{
+			'user': user.id,
+			'chat_id': chat_id,
+			'message': message
+			}
+		serializer = MessageSerializer(data=data)
 		if serializer.is_valid():
 			try :
-				chat = Chat.objects.filter(id=chat_id)
-				if Chat.user1 == user or Chat.user2 == user:
-					chat.discussion.add(serializer)
-					serializer.save()
+				chat = Chat.objects.get(id=chat_id)
+				if chat.user1 == user or chat.user2 == user:
+					new_message = Message.objects.create(user=user, chat_id=chat, message=message)
+					new_message.save()
+					chat.discussion.add(new_message)
 				else:
 					return Response({"error", "user are not in this chat"}, status=status.HTTP_403_FORBIDDEN)
 			except Chat.DoesNotExist:
 				return Response({"error", "chat not exist"}, status=status.HTTP_404_NOT_FOUND)
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ChatCreationView(APIView):
 	permission_classes = [IsAuthenticated]
@@ -78,9 +84,14 @@ class ChatCreationView(APIView):
 				try :
 					friendship = Friend_management.objects.get(friend1=user1, friend2=user2)
 				except Friend_management.DoesNotExist:
-					return Response({"error:", "friendship not exist"}, status=status.HTTP_400_BAD_REQUEST)
+					try :
+						friendship = Friend_management.objects.get(friend1=user2, friend2=user1)
+					except Friend_management.DoesNotExist:
+						return Response({"error:", "friendship not exist"}, status=status.HTTP_400_BAD_REQUEST)
 				if friendship.is_accepted == False:
 					return Response({"error:", "friendship not accepted"}, status=status.HTTP_400_BAD_REQUEST)
-				Chat.objects.create(user1=user1, user2=user2)
+				newChat = Chat.objects.create(user1=user1, user2=user2)
+				friendship.chat = newChat
+				friendship.save()
 				return Response({"message: ","chat created"}, status=status.HTTP_201_CREATED)
 		return Response({"error:", "chat already exist"}, status=status.HTTP_400_BAD_REQUEST)
